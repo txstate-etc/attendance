93fa38d272d140582a89f45e51807fb6e42e1c44
Jeff Snider <jsnider@txstate.edu>
2018-04-17 15:48:38 -0500

    Support for ssl cert replacement

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,9 @@ RUN apt-get update &&\
 	apt-get install wget ruby1.9.3 git build-essential libz-dev libxml2-dev libmysqlclient-dev apache2 apache2-dev libcurl4-openssl-dev libssl-dev -y &&\
 	gem install bundler &&\
 	wget https://raw.githubusercontent.com/txstate-etc/SSLConfig/master/SSLConfig-TxState.conf -O /etc/apache2/SSLConfig-TxState.conf &&\
-	mkdir -p /etc/pki/attendance &&\
-	openssl genrsa -out /etc/pki/attendance/attendance.key.pem 4096 &&\
-	openssl req -new -x509 -key /etc/pki/attendance/attendance.key.pem -out /etc/pki/attendance/attendance.cert.pem -sha256 -days 3650 -subj '/CN=localhost'
+	mkdir -p /ssl &&\
+	openssl genrsa -out /ssl/localhost.key.pem 4096 &&\
+	openssl req -new -x509 -key /ssl/localhost.key.pem -out /ssl/localhost.cert.pem -sha256 -days 3650 -subj '/CN=localhost'
 
 
 WORKDIR /tmp/docker
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ export CHECKIN_SECRET=${CHECKIN_SECRET:-`openssl rand -hex 64`}
 export OAUTH_SECRET=${OAUTH_SECRET:-`openssl rand -hex 64`}
 
 perl -i -pe 's/\Q{{CAS_BASE_URL}}\E/$ENV{CAS_BASE_URL}/' /tmp/docker/config/application.rb
-perl -i -pe 's/\Q{{WEB_HOSTNAME}}\E/$ENV{WEB_HOSTNAME}/' /etc/apache2/apache2.conf /tmp/docker/config/environments/production.rb
+perl -i -pe 's/\Q{{WEB_HOSTNAME}}\E/$ENV{WEB_HOSTNAME}/' /etc/apache2/apache2.conf
 
 perl -i -pe 's/\Q{{DB_DATABASE}}\E/$ENV{DB_DATABASE}/' /tmp/docker/config/database.yml
 perl -i -pe 's/\Q{{DB_USER}}\E/$ENV{DB_USER}/' /tmp/docker/config/database.yml
@@ -23,6 +23,9 @@ perl -i -pe 's/\Q{{DB_PORT}}\E/$ENV{DB_PORT}/' /tmp/docker/config/database.yml
 
 perl -i -pe 's/\Q{{CHECKIN_SECRET}}\E/$ENV{CHECKIN_SECRET}/' /tmp/docker/config/initializers/checkin_token.rb
 perl -i -pe 's/\Q{{OAUTH_SECRET}}\E/$ENV{OAUTH_SECRET}/' /tmp/docker/config/initializers/oauth_secret.rb
+
+perl -i -pe 's/\Q{{sslkeyfile}}\E/glob("\/ssl\/*.key.pem")/e' /etc/apache2/apache2.conf
+perl -i -pe 's/\Q{{sslcertfile}}\E/glob("\/ssl\/*.cert.pem")/e' /etc/apache2/apache2.conf
 
 cd /tmp/docker && runuser -u www-data rake assets:precompile
 
